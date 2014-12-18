3e167340168378a19df9db32b6e5e7bfaa43756c
Zemian Deng <zemian.deng@oracle.com>
2014-12-18 09:45:48 -0500

    Added servlet3 exmaple

---
+++ servlet3-example/src/main/java/zemian/servlet3example/web/IndexServlet.java
@@ -0,0 +1,20 @@
+package zemian.servlet3example.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
+}
